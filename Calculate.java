ac9561b49f7e2f183ed17f2a1917a153d40333f5
Karina Trofimova <karinatrofimova@gmail.com>
2018-06-24 16:35:09 +0200

    Math operations from pre-Junior xD

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,21 @@
 public class Calculate {
+
 	public static void main(String[] arg) {
-		System.out.println("Calculate");
-		System.out.print("GoGoGo");
+
+		System.out.println("Calculating...");
+
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int addition = first + second;
+		int subtraction = first - second;
+		int multiplication = first * second;
+		double division = (double)first / (double)second;
+		long involution = (long)first ^ (long)second;
+
+		System.out.println("addition = " + addition);
+		System.out.println("subtraction = " + subtraction);
+		System.out.println("multiplicaation = " + multiplication);
+		System.out.println("division = " + division);
+		System.out.println("involution = " + involution);
 	}
 }
